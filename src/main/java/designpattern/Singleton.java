e72768a15028fdaf005c217816fb82a785cfdb39
Benjamin Huang <bhuang@rms.com>
2018-10-18 16:45:00 -0700

    update

---
+++ src/main/java/designpattern/Singleton.java
@@ -0,0 +1,27 @@
+package designpattern;
+
+
+// Java program to create Thread Safe
+// Singleton class
+public class Singleton
+{
+    // private instance, so that it can be
+    // accessed by only by getInstance() method
+    private static Singleton instance;
+
+    private Singleton()
+    {
+        // private constructor
+    }
+
+    //synchronized method to control simultaneous access
+    synchronized public static Singleton getInstance()
+    {
+        if (instance == null)
+        {
+            // if instance is null, initialize
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
