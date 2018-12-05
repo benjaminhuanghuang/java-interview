f7445cb166afebc6cdd5101d37cd7419a5d4c9cf
Benjamin Huang <bhuang@rms.com>
2018-12-05 10:31:17 -0800

    update

---
+++ src/test/java/cn/huang/leetcode/LC_0890_FindandReplacePatternTest.java
@@ -0,0 +1,17 @@
+package cn.huang.leetcode;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class LC_0890_FindandReplacePatternTest {
+
+    @Test
+    public void findAndReplacePattern() {
+        LC_0890_FindandReplacePattern s = new LC_0890_FindandReplacePattern();
+        List<String> res = s.findAndReplacePattern(new String[]{"abc","deq","mee","aqq","dkd","ccc"}, "abb");
+
+    }
+}
