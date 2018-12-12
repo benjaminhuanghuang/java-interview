4e5511954179b5fd2869f51e0f80d0a2341e66b1
Benjamin Huang <bhuang@rms.com>
2018-12-11 23:21:51 -0800

    add comments

--- src/main/java/cn/huang/leetcode/LC_0415_AddStrings.java
+++ src/main/java/cn/huang/leetcode/LC_0415_AddStrings.java
@@ -2,6 +2,17 @@ package cn.huang.leetcode;
 /*
 415. Add Strings
 
+
+Given two non-negative numbers num1 and num2 represented as string, return the sum of num1 and num2.
+
+Note:
+
+The length of both num1 and num2 is < 5100.
+Both num1 and num2 contains only digits 0-9.
+Both num1 and num2 does not contain any leading zero.
+You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+refer to #2. Add Two Numbers
  */
 public class LC_0415_AddStrings {
     public String addStrings(String num1, String num2) {
--- src/main/java/cn/huang/leetcode/LC_0438_FindAllAnagramsinaString.java
+++ src/main/java/cn/huang/leetcode/LC_0438_FindAllAnagramsinaString.java
@@ -6,7 +6,40 @@ import java.util.List;
 
 /*
 438. Find All Anagrams in a String
+
+Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
+
+Strings consists of lowercase English letters only and the length of both strings s and p will not
+be larger than 20,100.
+
+The order of output does not matter.
+
+Example 1:
+
+Input:
+s: "cbaebabacd" p: "abc"
+
+Output:
+[0, 6]
+
+Explanation:
+The substring with start index = 0 is "cba", which is an anagram of "abc".
+The substring with start index = 6 is "bac", which is an anagram of "abc".
+Example 2:
+
+Input:
+s: "abab" p: "ab"
+
+Output:
+[0, 1, 2]
+
+Explanation:
+The substring with start index = 0 is "ab", which is an anagram of "ab".
+The substring with start index = 1 is "ba", which is an anagram of "ab".
+The substring with start index = 2 is "ab", which is an anagram of "ab".
  */
+
+
 public class LC_0438_FindAllAnagramsinaString {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> list = new ArrayList<>();
@@ -14,6 +47,7 @@ public class LC_0438_FindAllAnagramsinaString {
             return list;
 
         int[] hash = new int[128];
+        // p中每个字符出现多少次
         for (char c : p.toCharArray()) {
             hash[c]++;
         }
@@ -44,6 +78,9 @@ public class LC_0438_FindAllAnagramsinaString {
         return list;
     }
 
+    /*
+    https://www.youtube.com/watch?v=86fQQ7rVGxA
+    */
     public List<Integer> findAnagrams2(String s, String p) {
         List<Integer> ans = new ArrayList<>();
         if (s == null || s.length() == 0 || p == null || p.length() == 0)
