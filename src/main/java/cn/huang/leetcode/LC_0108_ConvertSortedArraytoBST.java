2c35f1cdadea19a09707b7e29ac8de2e722b2305
Benjamin Huang <bhuang@rms.com>
2018-12-24 12:57:44 -0800

    update

--- src/main/java/cn/huang/leetcode/LC_0108_ConvertSortedArraytoBST.java
+++ src/main/java/cn/huang/leetcode/LC_0108_ConvertSortedArraytoBST.java
@@ -11,9 +11,7 @@ public class LC_0108_ConvertSortedArraytoBST {
             return null;
 
         return helper(nums, 0, nums.length - 1);
-
     }
-
 
     private TreeNode helper(int[] nums, int left, int right) {
         if(left > right)
