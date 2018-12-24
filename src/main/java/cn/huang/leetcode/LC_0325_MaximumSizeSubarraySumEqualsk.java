c5836e38b6a4223469348172d15c97d3358abd62
Benjamin Huang <bhuang@rms.com>
2018-12-23 19:19:31 -0800

    update

---
+++ src/main/java/cn/huang/leetcode/LC_0325_MaximumSizeSubarraySumEqualsk.java
@@ -0,0 +1,53 @@
+package cn.huang.leetcode;
+
+import java.util.HashMap;
+
+/*
+325. Maximum Size Subarray Sum Equals k
+
+Given an array nums and a target value k, find the maximum length of a subarray that sums to k. If there isn't one,
+return 0 instead.
+
+Note:
+The sum of the entire nums array is guaranteed to fit within the 32-bit signed integer range.
+
+Example 1:
+Given nums = [1, -1, 5, -2, 3], k = 3,
+return 4. (because the subarray [1, -1, 5, -2] sums to 3 and is the longest)
+ */
+public class LC_0325_MaximumSizeSubarraySumEqualsk {
+    /*
+    https://www.youtube.com/watch?v=aYfwus5T3Bs
+     */
+    public int maxSubArrayLen(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        int max = 0;
+
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if (sum == k) {
+                max = Math.max(max, i + 1);
+            }
+
+            int diff = sum - k;
+
+            if (map.containsKey(diff)) {
+                max = Math.max(max, i - map.get(diff));
+            }
+
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+
+
+        return max;
+    }
+    /*
+    https://www.youtube.com/watch?v=mKXIH9GnhgU
+    */
+
+}
