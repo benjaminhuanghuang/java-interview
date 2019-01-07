fe84d9593c8f3cd56feab4d64a5607c49ed6c689
Benjamin Huang <bhuang@rms.com>
2019-01-07 11:39:20 -0800

    update

---
+++ src/main/java/cn/huang/leetcode/LC_0077_Combinations.java
@@ -0,0 +1,30 @@
+package cn.huang.leetcode;
+/*
+
+77. Combinations
+Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+
+Example:
+
+Input: n = 4, k = 2
+Output:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+
+
+ */
+public class LC_0077_Combinations {
+    /*
+    https://www.youtube.com/watch?v=-Nzrl6hCRKc
+
+    time O(n^ min{k, n-k}) , space O(n)
+
+     */
+
+}
