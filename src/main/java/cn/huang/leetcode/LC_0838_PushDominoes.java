a43ca7dbb1cb21c9d323c57ccac78bd11dc91131
Benjamin Huang <bhuang@rms.com>
2019-01-16 16:52:44 -0800

    update

--- src/main/java/cn/huang/leetcode/LC_0838_PushDominoes.java
+++ src/main/java/cn/huang/leetcode/LC_0838_PushDominoes.java
@@ -1,5 +1,7 @@
 package cn.huang.leetcode;
 
+import java.util.Arrays;
+
 /*
 838. Push Dominoes
 
@@ -40,6 +42,34 @@ Note:
  */
 public class LC_0838_PushDominoes {
     public String pushDominoes(String dominoes) {
+        dominoes = "L" + dominoes + "R";
+        char[] a = dominoes.toCharArray();
 
+        // This changes "R...R" to "RRRR" and changes "L...L" to "LLLL".
+        for (int i = 0, j; i < a.length; i++)
+            if (a[i] == 'L' || a[i] == 'R') {
+                for (j = i + 1; j < a.length && a[j] == '.'; j++) ; // process .
+                if (j < a.length && a[j] == a[i])
+                    Arrays.fill(a, i, j, a[i]);
+            }
+
+        // Then, handle the case for "R....L" separately.
+        for (int i = 0, j = 0; i < a.length; )
+            if (a[i] == 'R') {
+                for (j = i + 1; j < a.length && a[j] == '.'; j++) ;
+
+                if (j < a.length && a[j] == 'L') {
+                    for (int begin = i, end = j; begin < end; begin++, end--) {
+                        a[begin] = 'R';
+                        a[end] = 'L';
+                    }
+                }
+                i = j;
+            } else {
+                i++;
+            }
+
+        String str = String.valueOf(a);
+        return str.substring(1, str.length() - 1);
     }
 }
