d08cdcd2695fa6266dfd11bfcfeca368d1977869
Benjamin Huang <bhuang@rms.com>
2019-01-17 11:36:12 -0800

    update

--- src/main/java/practice/LC_Problem.java
+++ src/main/java/practice/LC_Problem.java
@@ -2,4 +2,14 @@ package practice;
 
 public class LC_Problem {
 
+
+    public static void main(String[] args) {
+        int[] testTable = { 0, 1, 2, 3, 4, 5, 6, 10 };
+        for (int input : testTable) {
+            System.out.println(input);
+        }
+    }
 }
+
+
+
