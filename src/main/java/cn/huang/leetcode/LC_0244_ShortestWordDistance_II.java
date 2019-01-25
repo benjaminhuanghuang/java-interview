1d6e732a2bda0f4b89574323fe91442411e8f016
Benjamin Huang <bhuang@rms.com>
2019-01-25 11:02:45 -0800

    update

---
+++ src/main/java/cn/huang/leetcode/LC_0244_ShortestWordDistance_II.java
@@ -0,0 +1,41 @@
+package cn.huang.leetcode;
+
+/*
+244	 Shortest Word Distance II
+
+This is a follow up of Shortest Word Distance. The only difference is now you are given the list of words and your method will be called repeatedly many times with different parameters. How would you optimize it?
+
+Design a class which receives a list of words in the constructor, and implements a method that takes two words word1 and word2 and return the shortest distance between these two words in the list.
+
+For example,
+Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+Given word1 = “coding”, word2 = “practice”, return 3.
+Given word1 = "makes", word2 = "coding", return 1.
+
+Note:
+You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
+ */
+public class LC_0244_ShortestWordDistance_II {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int index1 = -1;
+        int index2 = -1;
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < words.length; i++) {
+            String s = words[i];
+            if (word1.equals(s)) {
+                index1 = i;
+                if (index2 != -1)
+                    min = Math.min(min, index1 - index2);
+            } else if (word2.equals(s)) {
+                index2 = i;
+                if (index1 != -1)
+                    min = Math.min(min, index2 - index1);
+            }
+        }
+
+        return min;
+    }
+}
---
+++ src/main/java/cn/huang/leetcode/LC_0732_MyCalendar_III.java
@@ -0,0 +1,4 @@
+package cn.huang.leetcode;
+
+public class LC_0732_MyCalendar_III {
+}
