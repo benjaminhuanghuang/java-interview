1f6d786140ec397d66383c192505b3377a4d3486
Benjamin Huang <bhuang@rms.com>
2019-01-28 17:52:49 -0800

    update

--- src/main/java/cn/huang/leetcode/LC_0085_MaximalRectangle.java
+++ src/main/java/cn/huang/leetcode/LC_0085_MaximalRectangle.java
@@ -1,6 +1,7 @@
 package cn.huang.leetcode;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 /*
 85. Maximal Rectangle
@@ -65,6 +66,42 @@ public class LC_0085_MaximalRectangle {
             }
             for (int j = 0; j < n; j++) {
                 res = Math.max(res, (right[j] - left[j]) * height[j]);
+            }
+        }
+        return res;
+    }
+
+    /*
+    maintain a stack for height
+     */
+    public int maximalRectangle_stack(char[][] matrix) {
+        if (matrix == null || matrix.length == 0)
+            return 0;
+        int n = matrix[0].length;
+        int[] height = new int[n + 1];
+        height[n] = 0;
+        int res = 0;
+
+        for (int row = 0; row < matrix.length; row++) {
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < n + 1; i++) {
+                if (i < n) {
+                    if (matrix[row][i] == '1') {
+                        height[i]++;
+                    } else {
+                        height[i] = 0;
+                    }
+                }
+                if (stack.isEmpty() || height[stack.peek()] <= height[i]) {
+                    stack.push(i);
+                } else {
+                    while (!stack.isEmpty() && height[i] < height[stack.peek()]) {
+                        int h = stack.pop();
+                        int cur = height[h] * (stack.isEmpty() ? i : (i - stack.peek() - 1));
+                        res = Math.max(res, cur);
+                    }
+                    stack.push(i);
+                }
             }
         }
         return res;
