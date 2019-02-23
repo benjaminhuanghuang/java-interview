cd3c51e40b9de8c7ab6ca848794b9668ec54a904
Benjamin Huang <bhuang@rms.com>
2019-02-22 17:49:40 -0800

    update

--- src/main/java/cn/huang/leetcode/LC_0278_FirstBadVersion.java
+++ src/main/java/cn/huang/leetcode/LC_0278_FirstBadVersion.java
@@ -22,6 +22,24 @@ public class LC_0278_FirstBadVersion {
 
         return end;
     }
+    /*
+    can not pass 2147483647, 2147483647
+     */
+    public int firstBadVersion_huahua(int n) {
+        int start = 1;
+        int end = n + 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
 
     boolean isBadVersion(int version) {
         return false;
